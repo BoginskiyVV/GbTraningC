06872d355ff8485c9c3f8d4448e3b84ddcb7b734
BoginskiyVV <vov4ik84@gmail.com>
2022-08-20 23:45:54 +0300

    Добавлен файл с использованием фунции if-else

---
+++ ExpIfElse/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анна")
+{
+    Console.WriteLine("Привет и хорошего дня!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
+
+//{
+//Console.WriteLine("Привет и хорошего дня");
+//}
+//else
+//{
+//Console.Write("Здравствуйте, ");
+//  Console.WriteLine("username");
+//}
