9ed06a2255bb5d6a2dc3b2c0e3bdf0bba4fe1bf1
BoginskiyVV <vov4ik84@gmail.com>
2022-08-21 16:07:55 +0300

    Добавлено решение задачи 4

---
+++ Sem1Dz/Zadacha4/Zadacha4.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4. Напишие программу, которая на вход принимает три числа 
+//и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if(B > max)
+{
+    max = B;
+}
+
+if(C > max)
+{
+    max = C;
+    
+}
+Console.Write("max = ");
+Console.Write(max);
