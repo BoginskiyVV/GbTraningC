3fa21e712a5bb4bfcab5f9a5fbd244130917d3d6
BoginskiyVV <vov4ik84@gmail.com>
2022-08-21 23:18:42 +0300

    Поиск индекса массива по известному значению

---
+++ ExpMethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Поиск индекса массива по значению
+
+int[] array = { 11, 27, 36, 94, 50, 76, 48, 83, 27, 94 };
+
+int n = array.Length;
+int find = 94;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // остановит поиск на 1ом найденом индексе, 2ой надо ее отключить
+    }
+    index++;
+}
