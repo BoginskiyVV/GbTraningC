e2d00edc2b2965ec402432303b0a3e8fdda4b879
BoginskiyVV <vov4ik84@gmail.com>
2022-08-21 17:18:41 +0300

    Добавлено решение задачи № 6

---
+++ Sem1Dz/Zadacha6/zadacha6.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли 
+// число чётным, (делится без остатка)
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ostatokOtDeleniya = 0;
+
+ostatokOtDeleniya = number % 2;
+
+if(ostatokOtDeleniya == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.Write(" является чётным числом");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.Write(" является не чётным");
+}
