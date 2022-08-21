7a58b512cc7f09f1f2e8e562116c0c7be109e141
BoginskiyVV <vov4ik84@gmail.com>
2022-08-21 15:18:16 +0300

    Добавлена папка с решением задач 2, 4, 6, 8

---
+++ Sem1Dz/Zadacha2/Program.cs
@@ -0,0 +1,39 @@
+﻿// ЗАДАЧА 2
+// Виталий, если я правильно понимаю условия задачи, то числа необходимо вводить в терминал
+
+// Если условия указаны как в описании задачи то, тогда будет так
+// int numberA = 5; // 2, -9
+// int numberB = 7; // -9, -3
+// if(numberA > numberB)
+// {
+//     Console.Write("max = ");
+//     Console.Write(numberA);
+// }
+// else
+// {
+//     Console.Write("max = ");
+//     Console.Write(numberB);
+// }
+
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.Write(numberA);
+    Console.Write(" min = ");
+    Console.Write(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(numberB);
+    Console.Write(" min = ");
+    Console.Write(numberA);
+}
+
